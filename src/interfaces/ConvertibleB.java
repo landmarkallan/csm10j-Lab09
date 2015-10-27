43cad7690fa508d7e0186e959c4670f78bdb1f33
landmarkallan <gemrose1@gmail.com>
2015-10-27 16:50:40 -0700

    Create ConvertibleB.java

---
+++ src/interfaces/ConvertibleB.java
@@ -0,0 +1,34 @@
+package interfaces;
+
+class ConvertibleB implements Drivable
+{
+	// members:
+	private boolean isTopUp = true;
+	
+	public void PutTopUp()
+	{
+		if (this.isTopUp)
+		{
+			System.out.print("Top is already up");
+		}
+		else
+		{
+			System.out.print("Putting top up");
+			this.isTopUp = true;
+		}
+	}
+	
+	public void PutTopDown()
+	{
+		if (this.isTopUp)
+		{
+			System.out.print("Putting top down");
+			this.isTopUp = false;
+			
+		}
+		else
+		{
+			System.out.print("Top is already down");
+		}
+	}
+}
